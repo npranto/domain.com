61b90ec16ae7e288f4bfa894165868d61467ab96
Nazmuz Pranto <npranto@gmail.com>
2022-07-02 21:55:39 -0400

    adding UI layout for perfect domain grid

--- components/StartPerfectDomain/StartPerfectDomain.js
+++ components/StartPerfectDomain/StartPerfectDomain.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 import styles from "./StartPerfectDomain.module.css";
 
 const DEFAULT_TLDS = [
@@ -16,17 +17,44 @@ const DEFAULT_TLDS = [
 
 export default function StartPerfectDomain({ tlds = DEFAULT_TLDS }) {
   return (
-    <section className={styles.container}>
-      <h2>START WITH THE PERFECT DOMAIN</h2>
-      <h3>
+    <section className="flex flex-col max-w-screen-lg mx-auto my-0 px-4 md:px-10 py-20 space-y-4">
+      <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 md:text-5xl text-center">
+        START WITH THE PERFECT DOMAIN
+      </h2>
+      <h3 className="text-2xl font-thin tracking-tight text-gray-700 sm:text-3xl text-center">
         With more than 300 domain extensions, you'll find the one that fits just
         right.
       </h3>
-      <ul className="highlighted-tld-cards">
+      <ul className="highlighted-tld-cards space-y-1 m-0 list-none flex flex-col md:flex-row md:space-x-6">
         {tlds.map((tld) => {
           return (
-            <li key={tld.name} className="highlighted-tld-card">
-              {tld.name}
+            <li
+              key={tld.name}
+              className="highlighted-tld-card flex flex-col p-6 shadow-xl space-y-4 flex-grow md:w-96"
+            >
+              <h4 className="text-5xl font-bold tracking-tight text-gray-900">
+                <span className="red-dot text-red-600 mr-1">.</span>me
+              </h4>
+              <p className="tld-desc text-gray-500">
+                The perfect place to showcase who you are and what you're all
+                about.
+              </p>
+              <p className="starting-price text-xl text-gray-700">
+                Starting at $3.99/first year*
+              </p>
+              <form className="w-full domain-form flex justify-center relative">
+                <div className="domain-input w-full">
+                  <input
+                    type="text"
+                    name="domain"
+                    id="domain"
+                    className="border-2 block w-full   shadow-sm text-xl px-4 py-2 border-gray-300 rounded-md"
+                  />
+                </div>
+                <button type="submit" className="px-2 absolute right-0 h-full">
+                  <FaSearch className="text-xl text-red-600" />
+                </button>
+              </form>
             </li>
           );
         })}
